19425b85ef1f8e525aa4610c67019181ab88edf4
Edivan de Castro <edivandecastro@gmail.com>
2025-03-11 21:52:03 -0300

    Fix formatting and indentation in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,16 @@ import * as serviceWorker from "./serviceWorker"
 import { BrowserRouter } from "react-router-dom"
 import "./i18n"
 import { Provider } from "react-redux"
-import store  from "./store";
+import store from "./store";
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
     <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.Fragment>
 );
 
