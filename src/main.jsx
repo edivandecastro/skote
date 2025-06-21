5701c06c75cabe1014d4e5802b5227209b2bde26
Edivan de Castro <edivandecastro@gmail.com>
2025-06-21 12:19:27 -0300

    Wrap App component with QueryClientProvider and add ReactQueryDevtools

--- src/main.jsx
+++ src/main.jsx
@@ -6,14 +6,20 @@ import { BrowserRouter } from "react-router-dom"
 import "./i18n"
 import { Provider } from "react-redux"
 import store from "./store";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.Fragment>
     <Provider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </Provider>
   </React.Fragment>
 );
